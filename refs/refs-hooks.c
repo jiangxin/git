57d8c1f0b3a5e3d3ebaf9b301f2ce83e063a0a6a
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2018-10-22 18:17:11 +0800

    refs: add hooks before and after ref_transaction_commit
    Run pre-check and post-action hooks before and after refs transaction,
    so we can add permission check in pre-check, and update repo last
    modified time in post-action hook.
    
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

---
+++ refs/refs-hooks.c
@@ -0,0 +1,20 @@
+#include "../repository.h"
+#include "../cache.h"
+
+/*
+ * If this pre_check_hook failed (return no zero), ref transaction is denied,
+ * and user cannot write to the repository.
+ */
+int ref_transaction_pre_check_hook(struct strbuf *err) {
+	/* TODO: run permission check, etc. */
+	return 0;
+}
+
+/*
+ * After ref_transaction finished successfully, run this post_action hook,
+ * which will update a timestamp file to record last modified time.
+ */
+void ref_transaction_post_action_hook() {
+	/* TODO: update repo last-modified timestamp, etc. */
+	return;
+}
