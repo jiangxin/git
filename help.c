f3500e698fd40297d2e2634785529b76d49ca470
Jiang Xin <zhiyou.jx@alibaba-inc.com>
2026-01-16 09:53:10 +0800

    help: report on whether or not gettext is enabled
    When users report that Git has no localized output, we need to check not
    only their locale settings, but also whether Git was built with GETTEXT
    support in the first place.
    
    Expose this information via the existing build info output by adding a
    "gettext: enabled|disabled" line to `git version --build-options` (and
    therefore also to `git bugreport`). The status is derived from whether
    `NO_GETTEXT` is defined at build time.
    
    Signed-off-by: Jiang Xin <zhiyou.jx@alibaba-inc.com>

--- help.c
+++ help.c
@@ -799,6 +799,11 @@ void get_version_info(struct strbuf *buf, int show_build_options)
 
 		if (fsmonitor_ipc__is_supported())
 			strbuf_addstr(buf, "feature: fsmonitor--daemon\n");
+#if defined NO_GETTEXT
+		strbuf_addstr(buf, "gettext: disabled\n");
+#else
+		strbuf_addstr(buf, "gettext: enabled\n");
+#endif
 #if defined LIBCURL_VERSION
 		strbuf_addf(buf, "libcurl: %s\n", LIBCURL_VERSION);
 #endif
